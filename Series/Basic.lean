4af791d8bfd718f0b8eebc980490fb692e581eb7
Marco Devillers <marcodevillers@mac.home>
2026-09-25 01:15:17 +0200

    Complete Series.get

--- Series/Basic.lean
+++ Series/Basic.lean
@@ -1,1 +1,29 @@
-def hello := "world"
+/-
+Copyright (c) 2026 Marco Devillers. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Marco Devillers
+-/
+
+import Mathlib.Data.Set.Defs
+
+/-! prefix series -/
+structure Series (α : Type) where
+  carrier : Set (List α)
+  closed :
+    (∀ ⦃xs ys : List α⦄,
+      ys ∈ carrier →
+      xs <:+ ys →
+      xs ∈ carrier)
+  shared :
+    (∀ ⦃xs ys : List α⦄,
+      xs ∈ carrier →
+      ys ∈ carrier →
+      xs <:+ ys ∨ ys <:+ xs)
+
+noncomputable def Series.get {α : Type} (s : Series α) (n : Nat) : Option α := by
+  classical
+  if h : ∃ x, x ∈ s.carrier ∧ n < x.length then
+    let x := Classical.choose h
+    exact x[n]?
+  else
+    exact none
